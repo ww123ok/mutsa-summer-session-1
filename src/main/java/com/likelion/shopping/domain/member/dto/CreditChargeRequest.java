8eacb6f0606212ded526bef9c76b9d23b330232b
신현성 <ww123ok@naver.com>
2026-07-15 04:58:29 +0900

    [Fix] 크레딧 쏠림 버그 수정

--- src/main/java/com/likelion/shopping/domain/member/dto/CreditChargeRequest.java
+++ src/main/java/com/likelion/shopping/domain/member/dto/CreditChargeRequest.java
@@ -1,10 +1,12 @@
 package com.likelion.shopping.domain.member.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CreditChargeRequest {
     private int amount;
 }
