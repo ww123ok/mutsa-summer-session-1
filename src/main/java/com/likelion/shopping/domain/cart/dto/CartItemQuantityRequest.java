d808d045147bf7eeddfb344b7b2c5df31d04a4c6
신현성 <ww123ok@naver.com>
2026-07-19 03:04:24 +0900

    [Feat] 장바구니 최소 수량 검증 로직 추가

--- src/main/java/com/likelion/shopping/domain/cart/dto/CartItemQuantityRequest.java
+++ src/main/java/com/likelion/shopping/domain/cart/dto/CartItemQuantityRequest.java
@@ -1,10 +1,13 @@
 package com.likelion.shopping.domain.cart.dto;
 
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CartItemQuantityRequest {
+
+    @Min(value = 1, message = "장바구니 상품 수량은 1개 이상이어야 합니다.")
     private int quantity;
 }
